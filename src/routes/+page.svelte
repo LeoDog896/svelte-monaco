68aba484718b17baf3ad02282236ea22ecf00266
Tristan F <LeoDog896@hotmail.com>
2023-01-12 14:49:31 +0000

    docs(demo): add textarea to show reactive values
    (i don't know why i put the scope as "feat" before)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
     import "./style.css"
     import Monaco from "$lib/index"
+
+    let value = "const x = 5";
 </script>
 
-<Monaco language="ts" value="const x = 5;"></Monaco>
+<div id="editor">
+    <Monaco language="typescript" bind:value={value}></Monaco>
+</div>
+
+<textarea bind:value={value}></textarea>
+
+<style>
+    div#editor {
+        width: calc(100% - 2rem - 1px);
+        height: 80%;
+        border: 1px solid black;
+        max-height: 80%;
+    }
+</style>
